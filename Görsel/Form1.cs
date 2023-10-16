a54efc35f414b5cb3fe57a6f66187d7ab52d2f29
EmeksizlerAli <148108792+EmeksizlerAli@users.noreply.github.com>
2023-10-16 21:40:21 +0300

    Add files via upload

---
+++ Görsel/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace görsel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kullaniciadi = textBox1.Text;
+            int şifre = Convert.ToInt32(textBox2.Text);
+            if (kullaniciadi== "ali"  && şifre == 1256)
+            {
+                label3.Text = "Doğru";
+            }
+            else
+            {
+                label3.Text = "Yanlış";
+            }
+        }
+    }
+}
